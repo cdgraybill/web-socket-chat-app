56001ccb6d54e610890caa67bd51d18ee0b10e8d
cdgraybill <cdgraybill@gmail.com>
2023-10-23 21:43:26 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const io = require('socket.io')(3000);
+
+io.on('connection', socket => {
+    console.log("New User");
+    socket.emit('chat-message', "Hello World")
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
+})
